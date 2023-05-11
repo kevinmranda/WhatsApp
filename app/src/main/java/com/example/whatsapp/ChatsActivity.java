3352ba026229518295ad0f6d646630d0b7da4730
kevin <kevinmranda042@gmail.com>
2023-05-12 00:23:26 +0300

    Error solved, App working

--- app/src/main/java/com/example/whatsapp/ChatsActivity.java
+++ app/src/main/java/com/example/whatsapp/ChatsActivity.java
@@ -8,36 +8,32 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
 
-public class ChatsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+public class ChatsActivity extends AppCompatActivity implements BottomNavigationView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats);
 
-        BottomNavigationView navigationView =findViewById(R.id.bot_nav);
-
-        BottomNavigationView.OnNavigationItemSelectedListener(this::onNavigationItemSelected);
-        navigationView.setSelectedItemId(R.id.chats);
-
-
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bot_nav);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.chats);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.status:
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                overridePendingTransition(0,0);
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                overridePendingTransition(0, 0);
                 return true;
             case R.id.chats:
                 return true;
             case R.id.communities:
                 startActivity(new Intent(getApplicationContext(), CommunityActivity.class));
-                overridePendingTransition(0,0);
+                overridePendingTransition(0, 0);
                 return true;
         }
         return false;
--- app/src/main/java/com/example/whatsapp/CommunityActivity.java
+++ app/src/main/java/com/example/whatsapp/CommunityActivity.java
@@ -8,33 +8,32 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
 
-public class CommunityActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+public class CommunityActivity extends AppCompatActivity implements BottomNavigationView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community);
 
-        BottomNavigationView navigationView =findViewById(R.id.bot_nav);
-
-        BottomNavigationView.OnNavigationItemSelectedListener(this::onNavigationItemSelected);
-        navigationView.setSelectedItemId(R.id.communities);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bot_nav);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.communities);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.status:
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                overridePendingTransition(0,0);
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                overridePendingTransition(0, 0);
                 return true;
             case R.id.communities:
                 return true;
             case R.id.chats:
                 startActivity(new Intent(getApplicationContext(), ChatsActivity.class));
-                overridePendingTransition(0,0);
+                overridePendingTransition(0, 0);
                 return true;
         }
         return false;
